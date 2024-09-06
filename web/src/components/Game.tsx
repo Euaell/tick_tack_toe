b5785102b986be6208e9b0298b88aacbe1773357
Euaell <euaelmeko@gmail.com>
2024-09-06 11:15:00 +0300

    player history

--- web/src/components/Game.tsx
+++ web/src/components/Game.tsx
@@ -9,27 +9,42 @@ const styles = {
 }
 
 export default function Game(): React.ReactElement {
-    const [board, setBoard] = React.useState(Array(9).fill(null));
+    const [history, setHistory] = React.useState([Array(9).fill(null)]);
+    const [stepNumber, setStepNumber] = React.useState(0);
     const [xIsNext, setXIsNext] = React.useState(true);
-    const winner = calculateWinner(board);
+    const winner = calculateWinner(history[stepNumber]);
     
     function handleClick(i: number) {
-        const boardCopy = [...board];
-        if (winner || boardCopy[i]) return;
-        boardCopy[i] = xIsNext ? "X" : "O";
-        setBoard(boardCopy);
+        const timeInHistory = history.slice(0, stepNumber + 1);
+        const current = timeInHistory[stepNumber];
+        const squares = [...current];
+        if (winner || squares[i]) return;
+        squares[i] = xIsNext ? "X" : "O";
+        
+        setHistory([...timeInHistory, squares]);
+        setStepNumber(timeInHistory.length);
         setXIsNext(!xIsNext);
     }
 
-    // function jumpTo() {}
+    function jumpTo(step: number) {
+        setStepNumber(step);
+        setXIsNext(step % 2 === 0);        
+    }
 
     function renderMoves() {
-        return <button onClick={() => setBoard(Array(9).fill(null))}>Start Game</button>
+        return history.map((_step: any, move: number) => {
+            const destination = move ? `Go to move #${move}` : "Go to start";
+            return (
+                <li key={move}>
+                    <button onClick={() => jumpTo(move)}>{destination}</button>
+                </li>
+            )
+        })
     }
 
     return (
         <>
-            <Board squares={board} onClick={handleClick} />
+            <Board squares={history[stepNumber]} onClick={handleClick} />
             <div style={styles}>
                 <p>{winner ? "Winner: " + winner : "Next Player: " + (xIsNext ? "X" : "O")}</p>
                 {renderMoves()}
