25a9854ddc88ff3ca5d2da5d3fd722d3b1302643
Euaell <euaelmeko@gmail.com>
2024-09-06 10:19:15 +0300

    fix github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': '/src',
+export default defineConfig(() => {
+	const isGhPages = process.env.GITHUB_PAGES === 'true'
+	
+	return {
+		base: isGhPages ? '/tic_tac_toe/' : '/',
+		plugins: [react()],
+		resolve: {
+			alias: {
+				'@': '/src',
+			},
 		},
-	},
+	}
 })
