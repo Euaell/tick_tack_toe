84e82a644c3be605e1ccabf158e61b260af0c62a
Euaell <euaelmeko@gmail.com>
2024-09-07 00:14:58 +0300

    some fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as csurf from 'csurf';
 import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(csurf());
-  app.enableCors();
+
   app.use(helmet());
+  app.enableCors();
   await app.listen(8080);
 }
 bootstrap();
